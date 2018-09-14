fc1d69b090810aec17cb71982346d520281d78ea
Long <alphahoai@gmail.com>
2018-09-14 11:14:25 +0700

    commit index file

--- index.php
+++ index.php
@@ -4,4 +4,14 @@
  * User: Long
  * Date: 9/14/2018
  * Time: 10:51 AM
- */ 
+ */
+?>
+
+<html>
+<head>
+    <title>Nhóm 5 Web tiên tiến</title>
+</head>
+<body>
+    <h1>Nhóm 5 Web tiên tiến</h1>
+</body>
+</html>
