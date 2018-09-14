c2cc404136d42b7b8febed433a6473684143b04f
Long <alphahoai@gmail.com>
2018-09-14 11:23:51 +0700

    commit index file

--- index.php
+++ index.php
@@ -12,6 +12,18 @@
     <title>Nhóm 5 Web tiên tiến</title>
 </head>
 <body>
-    <h1>Nhóm 5 Web tiên tiến</h1>
+    <h1>Danh sách thành viên nhóm 5 Web tiên tiến</h1>
+    <ol>
+        <li><a href="http://fb.com/phah0ai" target="_blank" title="">Lê Ngọc Long</a></li>
+        <li><a href="javascript:missingFb()" title="">Bùi Như Minh</a></li>
+        <li><a href="javascript:missingFb()" title="">Đỗ Văn Thành</a></li>
+        <li><a href="javascript:missingFb()" title="">Đặng Văn Khang</a></li>
+    </ol>
+    <script>
+        function missingFb(){
+            alert('Ông điền facebook vào file này đi');
+            window.location.href='https://github.com/ALPhaHoai/webtt/blob/master/index.php';
+        }
+    </script>
 </body>
 </html>
